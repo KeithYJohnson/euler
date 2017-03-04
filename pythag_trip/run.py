16c1b763d57fea62a5aa7e5d4fb7f08f1514f7f8
Keith Johnson <keithyjohnson@gmail.com>
2017-03-04 13:05:46 -0500

    optimizing problem 9: pythagorean triplets

--- pythag_trip/run.py
+++ pythag_trip/run.py
@@ -5,7 +5,7 @@
 #
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
-
+import time
 max_c = 997
 
 def is_triplet(a,b,c):
@@ -16,8 +16,16 @@ def is_triplet(a,b,c):
 def check():
     for a in range(1, max_c + 1):
         print('current a: ', a)
-        for b in range(a + 1, 1000):
-            for c in range(b + 1, 1000 ):
-                if a + b + c == 1000 and is_triplet(a,b,c):
-                    return
+        for b in range(a + 1, 1000 - a):
+            #calculating c instead of iterating drops the time from
+            # # Time: 9.92964 seconds to  Time: 0.08036 seconds
+            c = 1000 - a - b
+            if c < b:
+                break
+            if is_triplet(a,b,c):
+                return
+
+start = time.time()
 check()
+end = time.time() - start
+print("Time: {:.5f} seconds".format(end))
